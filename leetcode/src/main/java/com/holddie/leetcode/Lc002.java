9caa47bd550b0ac714cc04d336aeafc195b7fc85
holddie <holddie@163.com>
2020-07-28 22:03:23 +0800

    feature: finish LeetCode 104

--- leetcode/src/main/java/com/holddie/leetcode/Lc002.java
+++ leetcode/src/main/java/com/holddie/leetcode/Lc002.java
@@ -30,8 +30,6 @@ public class Lc002 {
         }
         return retNode.next;
     }
-
-
 }
 
 class ListNode {
---
+++ leetcode/src/main/java/com/holddie/leetcode/Lc104.java
@@ -0,0 +1,81 @@
+package com.holddie.leetcode;//给定一个二叉树，找出其最大深度。 
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例： 
+//给定二叉树 [3,9,20,null,null,15,7]， 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最大深度 3 。 
+// Related Topics 树 深度优先搜索 
+// 👍 623 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.TreeMap;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Lc104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public int maxDepthWay1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode.left != null) {
+                    queue.offer(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.offer(treeNode.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
