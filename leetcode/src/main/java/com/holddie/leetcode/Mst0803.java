0e35ff66be88297a1914ce7a55f305fe86b1c3a2
holddie <holddie@163.com>
2020-07-31 09:12:48 +0800

    feature: add Mst0803

---
+++ leetcode/src/main/java/com/holddie/leetcode/Mst0803.java
@@ -0,0 +1,53 @@
+package com.holddie.leetcode;//魔术索引。 在数组A[0...n-1]中，有所谓的魔术索引，满足条件A[i] = i。给定一个有序整数数组，编写一种方法找出魔术索引，若有的话，在数组A中找
+//出一个魔术索引，如果没有，则返回-1。若有多个魔术索引，返回索引值最小的一个。 
+//
+// 示例1: 
+//
+//  输入：nums = [0, 2, 3, 4, 5]
+// 输出：0
+// 说明: 0下标的元素为0
+// 
+//
+// 示例2: 
+//
+//  输入：nums = [1, 1, 1]
+// 输出：1
+// 
+//
+// 提示: 
+//
+// 
+// nums长度在[1, 1000000]之间 
+// 
+// Related Topics 数组 二分查找 
+// 👍 33 👎 0
+
+
+class Mst0803 {
+    public int findMagicIndex(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int findMagicIndexWay1(int[] nums) {
+        return getAnswer(nums, 0, nums.length - 1);
+    }
+
+    private int getAnswer(int[] nums, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (right - left) / 2 + left;
+        if (mid == nums[mid]) {
+            return mid;
+        } else if (mid < nums[mid]) {
+            return getAnswer(nums, left, mid - 1);
+        } else {
+            return getAnswer(nums, mid + 1, right);
+        }
+    }
+}
---
+++ leetcode/src/test/java/com/holddie/leetcode/Mst0803Test.java
@@ -0,0 +1,62 @@
+package com.holddie.leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Mst0803Test {
+
+	private Mst0803 mst0803UnderTest;
+
+	@BeforeEach
+	void setUp() {
+		mst0803UnderTest = new Mst0803();
+	}
+
+	@Test
+	void testFindMagicIndex() {
+		// Setup
+		int[] nums = new int[]{0, 2, 3, 4, 5};
+
+		// Run the test
+		final int result = mst0803UnderTest.findMagicIndex(nums);
+
+		// Verify the results
+		assertEquals(0, result);
+	}
+
+	@Test
+	void testFindMagicIndex1() {
+		// Setup
+		int[] nums = new int[]{1, 1, 1};
+
+		// Run the test
+		final int result = mst0803UnderTest.findMagicIndex(nums);
+
+		// Verify the results
+		assertEquals(1, result);
+	}
+
+	@Test
+	void testFindMagicIndexWay1() {
+		// Setup
+		int[] nums = new int[]{1, 1, 1};
+		// Run the test
+		final int result = mst0803UnderTest.findMagicIndexWay1(nums);
+
+		// Verify the results
+		assertEquals(1, result);
+	}
+
+	@Test
+	void testFindMagicIndexWay11() {
+		// Setup
+		int[] nums = new int[]{0, 2, 3, 4, 5};
+		// Run the test
+		final int result = mst0803UnderTest.findMagicIndexWay1(nums);
+
+		// Verify the results
+		assertEquals(0, result);
+	}
+}
