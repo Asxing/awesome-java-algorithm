35883e28a7a18c4ec6a3b923d6aa44bbd7c26598
holddie <holddie@163.com>
2020-08-04 10:50:27 +0800

    feature: finish Lc207

---
+++ leetcode/src/main/java/com/holddie/leetcode/Lc207.java
@@ -0,0 +1,110 @@
+package com.holddie.leetcode;//你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。 
+//
+// 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1] 
+//
+// 给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？ 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: 2, [[1,0]] 
+//输出: true
+//解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。 
+//
+// 示例 2: 
+//
+// 输入: 2, [[1,0],[0,1]]
+//输出: false
+//解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 输入的先决条件是由 边缘列表 表示的图形，而不是 邻接矩阵 。详情请参见图的表示法。 
+// 你可以假定输入的先决条件中没有重复的边。 
+// 1 <= numCourses <= 10^5 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 图 拓扑排序 
+// 👍 433 👎 0
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Lc207 {
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<Integer>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+
+        for (int i = 0; i < numCourses && valid; i++) {
+            if (visited[i] == 0) {
+                dsf(i);
+            }
+        }
+        return valid;
+    }
+
+    private void dsf(int i) {
+        visited[i] = 1;
+        for (int v : edges.get(i)) {
+            if (visited[v] == 0) {
+                dsf(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[i] = 2;
+    }
+
+    int[] indeg;
+    List<List<Integer>> edgesTwo;
+
+    public boolean canFinishWayTwo(int numCourses, int[][] prerequisites) {
+        edgesTwo = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; i++) {
+            edgesTwo.add(new ArrayList<Integer>());
+        }
+        indeg = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edgesTwo.get(info[1]).add(info[0]);
+            ++indeg[info[0]];
+        }
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            ++visited;
+            int u = queue.poll();
+            for (int v : edgesTwo.get(u)) {
+                --indeg[v];
+                if (indeg[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+        return visited == numCourses;
+    }
+}
---
+++ leetcode/src/test/java/com/holddie/leetcode/Lc207Test.java
@@ -0,0 +1,69 @@
+package com.holddie.leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Lc207Test {
+
+	private Lc207 lc207UnderTest;
+
+	@BeforeEach
+	void setUp() {
+		lc207UnderTest = new Lc207();
+	}
+
+	@Test
+	void testCanFinish() {
+		// Setup
+		final int[][] prerequisites = new int[][]{{1, 0}};
+		int numCourses = 2;
+
+		// Run the test
+		final boolean result = lc207UnderTest.canFinish(numCourses, prerequisites);
+
+		// Verify the results
+		assertTrue(result);
+	}
+
+	@Test
+	void testCanFinish1() {
+		// Setup
+		final int[][] prerequisites = new int[][]{{1, 0}, {0, 1}};
+		int numCourses = 2;
+
+		// Run the test
+		final boolean result = lc207UnderTest.canFinish(numCourses, prerequisites);
+
+		// Verify the results
+		assertFalse(result);
+	}
+
+	@Test
+	void testCanFinishWayTwo() {
+		// Setup
+		final int[][] prerequisites = new int[][]{{1, 0}};
+		int numCourses = 2;
+
+		// Run the test
+		final boolean result = lc207UnderTest.canFinishWayTwo(numCourses, prerequisites);
+
+		// Verify the results
+		assertTrue(result);
+	}
+
+	@Test
+	void testCanFinishWayTwo1() {
+		// Setup
+		final int[][] prerequisites = new int[][]{{1, 0}, {0, 1}};
+		int numCourses = 2;
+
+		// Run the test
+		final boolean result = lc207UnderTest.canFinishWayTwo(numCourses, prerequisites);
+
+		// Verify the results
+		assertFalse(result);
+	}
+}
