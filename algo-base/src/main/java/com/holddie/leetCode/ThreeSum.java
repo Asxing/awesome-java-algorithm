683066e67a314f9193cc8cee4845a64507bfd76d
holddie <holddie@163.com>
2021-01-25 13:00:16 +0800

    feature: 添加算法大纲

--- algo-base/src/main/java/com/holddie/leetCode/ThreeSum.java
+++ algo-base/src/main/java/com/holddie/leetCode/ThreeSum.java
@@ -1,41 +1,46 @@
 package com.holddie.leetCode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ThreeSum {
     public static void main(String[] args) {
-        int[] num = {-2, 0, 1, 1, 2};
-        System.out.println(threeSumClosest(num, 0));
+        int[] nums = {-2, 0, 1, 1, 2};
+        System.out.println(threeSum(nums, 4));
     }
 
-    public static int threeSumClosest(int[] num, int target) {
-        // 数组中3数之和与target的差值
-        int dv = Integer.MAX_VALUE;
-        // 返回值
-        int res = Integer.MAX_VALUE;
+    public static ArrayList<ArrayList<Integer>> threeSum(int[] num, int target) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (num.length < 3 || num == null) {
+            return res;
+        }
         Arrays.sort(num);
-        for (int i = 0; i < num.length - 2; i++) {
-            if (i != 0 && num[i] == num[i - 1]) {
-                i++;
-            }
-            int left = i + 1;
-            int right = num.length - 1;
-            while (left < right) {
-                int sum = num[left] + num[right];
-                if (sum + num[i] == target) {
-                    return target;
-                } else if (sum + num[i] < target) {
-                    if (dv > Math.abs(sum + num[i] - target)) {
-                        dv = Math.abs(sum + num[i] - target);
-                        res = sum + num[i];
+        for (int i = 0; i <= num.length - 3; i++) {
+            if (i == 0 || num[i] != num[i - 1]) {
+                int low = i + 1;
+                int high = num.length - 1;
+                while (low < high) {
+                    int sum = num[i] + num[low] + num[high];
+                    if (sum == target) {
+                        ArrayList<Integer> unit = new ArrayList<>();
+                        unit.add(num[i]);
+                        unit.add(num[low]);
+                        unit.add(num[high]);
+                        res.add(unit);
+                        low++;
+                        high--;
+
+                        while (low < high && num[low] == num[low - 1]) {
+                            low++;
+                        }
+                        while (low < high && num[high] == num[high + 1]) {
+                            high--;
+                        }
+                    } else if (sum > target) {
+                        high--;
+                    } else {
+                        low++;
                     }
-                    left++;
-                } else {
-                    if (dv > Math.abs(sum + num[i] - target)) {
-                        dv = Math.abs(sum + num[i] - target);
-                        res = sum + num[i];
-                    }
-                    right--;
                 }
             }
         }
